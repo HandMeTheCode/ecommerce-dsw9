366024796449383be60216194e9af18412dc9d46
HandMeTheCode <cristian.andrade@utp.ac.pa>
2026-05-08 01:13:11 +0000

    feat: controladores y rutas completos + updt db.js

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ function getSslConfig() {
   if (fs.existsSync(certPath)) {
     return { ssl: { ca: fs.readFileSync(certPath) } };
   }
-  return {};
+  return { ssl: { rejectUnauthorized: false } }; // ← only line changed
 }
 
 const sequelize = new Sequelize(
